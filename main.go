6b877fb5d9e28940efdd4b81ce6a39bb1cf91e7f
icobani <ibrahim@imaconsult.com>
2025-05-18 19:15:30 +0300

    `Ana Go dosyası oluşturuldu ve yapılandırma ile veritabanı başlangıç işlemleri eklendi`

---
+++ main.go
@@ -0,0 +1,34 @@
+/*
+	B1 Yönetim Sistemleri Yazılım ve Danışmanlık Ltd. Şti.
+	User        : ICI
+	Name        : Ibrahim COBANI
+	Date        : 18.05.2025
+	Time        : 17:47
+	Notes       :
+
+
+*/
+
+package main
+
+import (
+	"log"
+
+	"github.com/icobani/GolangGorm/config"
+)
+
+var (
+	Version string
+)
+
+func main() {
+
+	if err := config.InitConfigFile(); err != nil {
+		log.Println("Error: ", err)
+	}
+
+	if err := config.InitDB(); err != nil {
+		log.Println("Error: ", err)
+	}
+
+}
