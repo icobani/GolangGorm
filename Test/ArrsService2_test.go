c109ac7405bba288a704b09c5d7e0c99978761de
taham <tahamusacapkur@gmail.com>
2025-05-25 18:17:27 +0300

    Add user login functionality with request and response types

--- Test/ArrsService2_test.go
+++ Test/ArrsService2_test.go
@@ -46,4 +46,32 @@ func TestArrsPlanning2Test(t *testing.T) {
 		log.Println("User Created:")
 		service.PrintPrettyStruct(user)
 	}
+
+}
+
+func LoginUser(t *testing.T) {
+	if err := config.InitConfigFile(); err != nil {
+		log.Println("Error: ", err)
+		return
+	}
+	if err := config.InitDB(); err != nil {
+		log.Println("Error: ", err)
+		return
+	}
+	service := ArrsPlanningService.NewArrsPlanningService(config.Params, config.DB)
+
+	userService := service.LoginUser(config.DB)
+
+	// Giriş denemesi
+	req := ArrsPlanningService.LoginRequest{
+		Username: "icobani",
+		Password: "112233",
+	}
+	resp, err := userService.Login(req)
+	if err != nil {
+		t.Errorf("Giriş başarısız: %v", err)
+	} else {
+		t.Logf("Başarılı giriş: %s", resp.Message)
+	}
+
 }
