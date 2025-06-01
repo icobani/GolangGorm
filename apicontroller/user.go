ccaa4a78a215988ac8c4d0be0a49a57f1b120893
ucrnsemin <ucarenesemin@gmail.com>
2025-06-01 19:17:14 +0300

    "PutChangeMyPassword fonksiyonu eklendi, kullanıcılar için şifre değiştirme desteği sağlandı."

--- apicontroller/user.go
+++ apicontroller/user.go
@@ -30,3 +30,21 @@ func GetUsers(c *gin.Context) {
 	c.JSON(http.StatusOK, users)
 
 }
+
+func PutChangeMyPassword(c *gin.Context) {
+	var req ArrsPlanningService.ChangePasswordRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request data",
+			"details": err.Error()})
+		return
+	}
+
+	s := ArrsPlanningService.NewArrsPlanningService(config.Params, config.DB)
+	if err := s.ChangeMyPassword(req); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to change password",
+			"details": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Password changed successfully"})
+}
