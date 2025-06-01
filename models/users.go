9400ba382eb1bfb02191ef7e48ef2a49435e5faa
ucrnsemin <ucarenesemin@gmail.com>
2025-06-01 19:46:13 +0300

    "User modeline adres bilgisi için BaseAddress tipi eklendi."

--- models/users.go
+++ models/users.go
@@ -35,16 +35,26 @@ func (u UserStatus) String() string {
 	}
 }
 
+type BaseAddress struct {
+	Name     string `json:"name"`
+	Address  string `json:"address"`
+	City     string `json:"city"`
+	Country  string `json:"country"`
+	ZipCode  string `json:"zip_code"`
+	District string `json:"district"`
+}
+
 type User struct {
-	ID       uint64     `json:"id" gorm:"primaryKey"` // Kullanıcı Kimlik numarası
-	FullName string     `json:"full_name"`            // Ad Soyad
-	UserName string     `json:"user_name"`            // Kullanıcı Adı
-	Password string     `json:"-"`                    // Şifre
-	Email    string     `json:"email"`                // Email Adresi
-	Phone    string     `json:"phone"`                // Telefon
-	Status   UserStatus `json:"status"`               // Durum
-	Lat      float32    `json:"lat"`                  // Kullanıcının son konumu enlem
-	Lng      float32    `json:"lng"`                  // Kullanıcının son konumu boylam
-	Balance  float32    `json:"balance"`              // Kullanıcının bakiyesi
-	Debit    float32    `json:"debit"`                // Kullanıcın Borcu
+	ID       uint64      `json:"id" gorm:"primaryKey"`                            // Kullanıcı Kimlik numarası
+	FullName string      `json:"full_name"`                                       // Ad Soyad
+	UserName string      `json:"user_name"`                                       // Kullanıcı Adı
+	Password string      `json:"-"`                                               // Şifre
+	Email    string      `json:"email"`                                           // Email Adresi
+	Phone    string      `json:"phone"`                                           // Telefon
+	Status   UserStatus  `json:"status"`                                          // Durum
+	Lat      float32     `json:"lat"`                                             // Kullanıcının son konumu enlem
+	Lng      float32     `json:"lng"`                                             // Kullanıcının son konumu boylam
+	Balance  float32     `json:"balance"`                                         // Kullanıcının bakiyesi
+	Debit    float32     `json:"debit"`                                           // Kullanıcın Borcu
+	Address  BaseAddress `json:"address" gorm:"embedded;embeddedPrefix:address_"` // Kullanıcının adresi
 }
