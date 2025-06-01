e680a7acee3561caa9d930ae1795bf3e83ca25af
ucrnsemin <ucarenesemin@gmail.com>
2025-06-01 18:37:33 +0300

    Yeni "/test" endpointleri eklendi (GET, POST, PUT, DELETE) ve UserRoot entegrasyonu yapıldı.

--- initApi.go
+++ initApi.go
@@ -13,6 +13,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/icobani/GolangGorm/apiroots"
 	"log"
 	"net/http"
 
@@ -31,6 +32,63 @@ func InitApi() {
 			"message": "pong",
 		})
 	})
+
+	app.GET("/test", func(c *gin.Context) {
+
+		filter := c.Query("filter")
+		log.Println("Received filter query parameter:", filter)
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "test",
+			"filter":  filter,
+		})
+
+	})
+
+	app.POST("/test", func(c *gin.Context) {
+		var requestData map[string]interface{}
+		if err := c.BindJSON(&requestData); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request data"})
+			return
+		}
+
+		log.Println("Received data:", requestData)
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "test",
+			"data":    requestData,
+		})
+	})
+
+	app.PUT("/test", func(c *gin.Context) {
+		var requestData map[string]interface{}
+		if err := c.BindJSON(&requestData); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request data"})
+			return
+		}
+
+		log.Println("Received data:", requestData)
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "test",
+			"data":    requestData,
+		})
+	})
+
+	app.DELETE("/test/:id", func(c *gin.Context) {
+
+		id := c.Param("id")
+		log.Println("Received delete request for ID:", id)
+
+		c.JSON(http.StatusOK, gin.H{
+			"message": "test",
+			"id":      id,
+		})
+
+	})
+
+	api := app.Group("/api")
+	apiroots.UserRoot(api)
 
 	baseURL := fmt.Sprintf(":%s", port)
 	err = app.Run(baseURL)
